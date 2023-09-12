178fe0cdb8fad7343e81f8cd36fbf312f8a96dbb
Jeremy Nixon <quay.truss.0b@icloud.com>
2023-09-11 22:20:05 -0400

    update dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": "plugin:react/recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "2022",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
